4c484dfd9eb78e36a117b5d4f262298940c305e7
carvalhotaty <soarescarvalhotatiane@gmail.com>
2023-12-26 21:17:56 -0400

    react2

--- src/pages/Inicio.jsx
+++ src/pages/Inicio.jsx
@@ -1,14 +1,40 @@
-import React from 'react'
-import Menu from '../Components/Menu'
-import { Button } from '@mui/material'
+import React from 'react';
+import Menu from '../Components/Menu';
+import { Box, Button, FormControl, InputLabel, MenuItem, Select, TextField } from '@mui/material';
+import { Home } from '@mui/icons-material';
+
 
 const Inicio = () => {
   return (
     <div>
 
-      
+     
       <Menu/>
-      <Button variant="contained">Clique aqui</Button> |
+      <br></br>
+      <Button variant="contained"
+      disableElevation
+      size="large"
+      color="success"
+      startIcon={<Home/>}
+      endIcon={<Home/>}
+      >
+        Clique aqui 
+      </Button> 
+      <br></br>
+      <br></br>
+<TextField id="Digite Seu Nome" label="Escreva Seu Nome" variant='outlined'/>
+
+<br></br>
+<br></br>
+
+<InputLabel id="label-sexo">Sexo</InputLabel>
+<Select  labelId="label-sexo" label="sexo">
+<MenuItem>Masculino</MenuItem>
+<MenuItem>Feminino</MenuItem>
+</Select>
+<br />
+<br />
+
       Início
     </div>
   )
